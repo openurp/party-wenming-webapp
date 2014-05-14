31d6341b3df9986cf6ea1ce6f6f57e926c646885
duantihua <duantihua@gmail.com>
2014-05-14 14:50:19 +0800

    add good thing

---
+++ wenming/src/main/java/org/openurp/webapp/apps/party/wenming/model/GoodPost.java
@@ -0,0 +1,13 @@
+package org.openurp.webapp.apps.party.wenming.model;
+
+
+/**
+ * 文明示范岗
+ * @author chaostone
+ *
+ */
+public class GoodPost  extends AbstractGoodThing{
+
+  private static final long serialVersionUID = -8549949502746276473L;
+
+}
