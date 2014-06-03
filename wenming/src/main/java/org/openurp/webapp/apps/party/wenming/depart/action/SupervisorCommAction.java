f0fb0cdef4a8daed6b9f85ef64a1eab9ced421d6
mychii <mychii@qq.com>
2014-06-03 10:54:19 +0800

    Supervisor auto login

--- wenming/src/main/java/org/openurp/webapp/apps/party/wenming/depart/action/SupervisorCommAction.java
+++ wenming/src/main/java/org/openurp/webapp/apps/party/wenming/depart/action/SupervisorCommAction.java
@@ -1,5 +1,8 @@
 package org.openurp.webapp.apps.party.wenming.depart.action;
 
+import java.util.List;
+
+import org.beangle.security.blueprint.User;
 import org.openurp.webapp.apps.party.wenming.depart.model.Supervisor;
 
 
@@ -7,7 +10,16 @@ public class SupervisorCommAction extends WenMingAction{
   public static final String SUPERVISOR_ID = "SUPERVISOR_ID";
 
   protected Integer getSupervisorId() {
-    return (Integer) getSession().get(SUPERVISOR_ID);
+    Integer supervisorId = (Integer) getSession().get(SUPERVISOR_ID);
+    if(supervisorId == null && getUserId() != null){
+      User user = getUrpUser();
+      List<Supervisor> list = entityDao.get(Supervisor.class, "name", user.getName());
+      if(!list.isEmpty()){
+        supervisorId = list.get(0).getId();
+        getSession().put(SUPERVISOR_ID, supervisorId);
+      }
+    }
+    return supervisorId;
   }
   
   protected Supervisor getSupervisor(){
